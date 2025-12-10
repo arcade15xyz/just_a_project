679768a0e6407a24c4dc1e0816f3b5dce32ef0ee
arcade15xyz <arcade15xyz@gmail.com>
2025-12-10 16:41:09 +0530

    tapping on card working

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class RecipeApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final ThemeData theme = ThemeData();
+    final ThemeData theme = ThemeData.light(useMaterial3: false);
     return MaterialApp(
       title: 'Recipe Calculator',
       theme: theme.copyWith(
@@ -35,14 +35,58 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.title), centerTitle: true,),
-      body: SafeArea(child: ListView.builder(
-        itemCount: Recipe.samples.length,
-        itemBuilder: (context, int index){
-          return Text(Recipe.samples[index].label);
-        },
+      appBar: AppBar(title: Text(widget.title), centerTitle: true),
+      body: SafeArea(
+        child: ListView.builder(
+          itemCount: Recipe.samples.length,
+          itemBuilder: (context, int index) {
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      //recipe details
+                      return Text("Detail Page");
+                    },
+                  ),
+                );
+              },
+              child: Column(
+                children: [
+                  buildRecipeCard(Recipe.samples[index]),
+                  SizedBox(height: 10),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
 
-      )),
+  ///this is for building the Recipe card.
+  Widget buildRecipeCard(Recipe recipe) {
+    return Card(
+      elevation: 2,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: <Widget>[
+            Image(image: AssetImage(recipe.imageUrl)),
+            SizedBox(height: 14),
+            Text(
+              recipe.label,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w700,
+                fontFamily: 'Palatino',
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
